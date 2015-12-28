ad41afc8a8accbf43646ad4002ccec65868c933b
Maulia Harjono <ulik.295fk@gmail.com>
2015-12-28 23:07:43 +0700

    Conflict

--- app/src/main/java/mad/tubes/emergencycalls/MainActivity.java
+++ app/src/main/java/mad/tubes/emergencycalls/MainActivity.java
@@ -2,13 +2,12 @@ package mad.tubes.emergencycalls;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+//import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends Activity {
 
---
+++ app/src/main/java/mad/tubes/emergencycalls/TheMaps.java
@@ -0,0 +1,34 @@
+package mad.tubes.emergencycalls;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * Created by user on 12/28/2015.
+ */
+public class TheMaps extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
